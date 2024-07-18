b84f9bdbb65f512fc94ac49b484613fd15c4ccfa
Rivaldo Nicolas <rivaldonicolasfabian@gmail.com>
2024-07-18 00:49:39 -0500

    creando menu de navegacion y pie de pagina

--- elBorrachoFiel/sitioweb/urls.py
+++ elBorrachoFiel/sitioweb/urls.py
@@ -4,9 +4,9 @@ from . import  views
 urlpatterns = [
     path("",views.inicio, name=("inicio")),
     path("nosotros/",views.nosotros, name=("nosotros")),
-    path("cerbeza/",views.cerbeza, name=("cerbeza")),
+    path("productos/",views.productos, name=("productos")),
     path("contactos/",views.contactos, name=("contactos")),
     path("carrito/",views.carrito, name=("carrito")),
     path("iniciarSesion/",views.iniciarSesion, name=("iniciarSesion")),
-    path("registro/",views.registro, name=("registro")),
+    path("registrarse/",views.registrarse, name=("registrarse")),
 ]
--- elBorrachoFiel/sitioweb/views.py
+++ elBorrachoFiel/sitioweb/views.py
@@ -4,17 +4,17 @@ from django.shortcuts import render
 def inicio(request):
     return render(request,"index.html")
 def nosotros(request):
-    return render(request,"about.html")
-def cerbeza(request):
-    return render(request,"skills.html")
+    return render(request,"nosotros.html")
+def productos(request):
+    return render(request,"productos.html")
 def contactos(request):
-    return render(request,"proyectos.html")
+    return render(request,"contactos.html")
 def carrito(request):
     return render(request,"carrito.html")
 def iniciarSesion(request):
-    return render(request,"contactos.html")
-def registro(request):
-    return render(request,"contactos.html")
+    return render(request,"iniciarSesion.html")
+def registrarse(request):
+    return render(request,"registrarse.html")
 
 #EJEMPLO RENDERIZAR UNA DICCIONARIO EN UNA LISTA
 # def usuariosVista(request):
